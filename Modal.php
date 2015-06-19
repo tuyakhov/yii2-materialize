6231c0a0e7380115f31ac08ff9edb1333e76f2af
Anton Tuyakhov <atuyakhov@gmail.com>
2015-06-19 18:53:50 +0300

    modal

---
+++ Modal.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * @author Anton Tuyakhov <atuyakhov@gmail.com>
+ */
+
+namespace tuyakhov\materialize;
+
+
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+
+class Modal extends Widget
+{
+    /**
+     * Modals Structure css classes
+     */
+    const TYPE_FIXED_FOOTER = 'modal-fixed-footer';
+    const TYPE_BOTTOM_SHEET = 'bottom-sheet';
+
+    /**
+     * Modal Structure
+     * @see http://materializecss.com/modals.html
+     * @var string
+     */
+    public $modalType;
+    /**
+     * @var string the footer content in the modal window.
+     */
+    public $footer;
+    /**
+     * @var string additional footer options
+     * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
+     * @since 2.0.1
+     */
+    public $footerOptions;
+    /**
+     * @var array|false the options for rendering the close button tag.
+     * The close button is displayed in the header of the modal window. Clicking
+     * on the button will hide the modal window. If this is false, no close button will be rendered.
+     *
+     * The following special options are supported:
+     *
+     * - tag: string, the tag name of the button. Defaults to 'button'.
+     * - label: string, the label of the button. Defaults to 'Close'.
+     *
+     * The rest of the options will be rendered as the HTML attributes of the button tag.
+     * Please refer to the [Modal plugin help](http://materializecss.com/modals.html)
+     * for the supported HTML attributes.
+     */
+    public $closeButton = [];
+    /**
+     * @var array the options for rendering the toggle button tag.
+     * The toggle button is used to toggle the visibility of the modal window.
+     * If this property is false, no toggle button will be rendered.
+     *
+     * The following special options are supported:
+     *
+     * - tag: string, the tag name of the button. Defaults to 'button'.
+     * - label: string, the label of the button. Defaults to 'Show'.
+     *
+     * The rest of the options will be rendered as the HTML attributes of the button tag.
+     * Please refer to the [Modal plugin help](http://materializecss.com/modals.html)
+     * for the supported HTML attributes.
+     */
+    public $toggleButton = false;
+
+
+    /**
+     * Initializes the widget.
+     */
+    public function init()
+    {
+        parent::init();
+
+        $this->initOptions();
+
+        echo $this->renderToggleButton() . "\n";
+        echo Html::beginTag('div', $this->options) . "\n";
+        echo Html::beginTag('div', ['class' => 'modal-content']) . "\n";
+    }
+
+    /**
+     * Renders the widget.
+     */
+    public function run()
+    {
+        echo "\n" . $this->renderFooter();
+        echo "\n" . Html::endTag('div'); // modal-content
+        echo "\n" . Html::endTag('div');
+
+        $this->registerPlugin('leanModal');
+    }
+
+    /**
+     * Renders the HTML markup for the footer of the modal
+     * @return string the rendering result
+     */
+    protected function renderFooter()
+    {
+        if ($this->footer !== null) {
+            Html::addCssClass($this->footerOptions, 'modal-footer');
+            if ($this->closeButton !== false) {
+                $this->footer .= $this->renderCloseButton();
+            }
+            return Html::tag('div', "\n" . $this->footer . "\n", $this->footerOptions);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Renders the toggle button.
+     * @return string the rendering result
+     */
+    protected function renderToggleButton()
+    {
+        if (($toggleButton = $this->toggleButton) !== false) {
+            $tag = ArrayHelper::remove($toggleButton, 'tag', 'button');
+            $label = ArrayHelper::remove($toggleButton, 'label', 'Show');
+            if ($tag === 'button' && !isset($toggleButton['type'])) {
+                $toggleButton['type'] = 'button';
+            }
+
+            return Html::tag($tag, $label, $toggleButton);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Renders the close button.
+     * @return string the rendering result
+     */
+    protected function renderCloseButton()
+    {
+        if (($closeButton = $this->closeButton) !== false) {
+            $tag = ArrayHelper::remove($closeButton, 'tag', 'button');
+            $label = ArrayHelper::remove($closeButton, 'label', 'Close');
+            if ($tag === 'button' && !isset($closeButton['type'])) {
+                $closeButton['type'] = 'button';
+            }
+
+            return Html::tag($tag, $label, $closeButton);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Initializes the widget options.
+     * This method sets the default values for various options.
+     */
+    protected function initOptions()
+    {
+        if ($this->modalType !== null) {
+            Html::addCssClass($this->options, $this->modalType);
+        }
+        Html::addCssClass($this->options, 'modal');
+
+        if ($this->clientOptions !== false) {
+            $this->clientOptions = array_merge(['show' => false], $this->clientOptions);
+        }
+
+        if ($this->closeButton !== false) {
+            Html::addCssClass($this->closeButton, 'modal-close');
+        }
+
+        if ($this->toggleButton !== false) {
+            Html::addCssClass($this->toggleButton, 'modal-trigger');
+            if (!isset($this->toggleButton['data-target']) && !isset($this->toggleButton['href'])) {
+                $this->toggleButton['data-target'] = '#' . $this->options['id'];
+            }
+        }
+    }
+}
